79bcbfe9b91a58c8823844edbf7aa5ff7e8f699a
Granson Oyombe <oyombegranson@gmail.com>
2018-10-13 12:33:09 +0300

    Added https for seamless communication to the heroku APIs

--- ui/js/login.js
+++ ui/js/login.js
@@ -41,7 +41,7 @@ function registerUser(username, password, type){
         mode: "cors" 
     }
 
-    fetch("http://fast-food-fast-c4.herokuapp.com/auth/login",options)
+    fetch("https://fast-food-fast-c4.herokuapp.com/auth/login",options)
     .then((response) => response.json())
       .then((result) => {
           if(result.status == 0){
